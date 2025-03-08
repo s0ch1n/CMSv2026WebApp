c449d3ab242c60af74b699b843625bad7280a577
MOHAMMED AFSAL <mohafsal890@gmail.com>
2025-03-08 10:21:56 +0530

    Admin changes added

--- DTO/ReferralRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CMSv2026WebApp.DTO
-{
-    public class ReferralRequest
-    {
-        public int AppointmentId { get; set; }
-        public int ReferringDoctorId { get; set; }
-        public int ReferredDoctorId { get; set; }
-    }
-}
