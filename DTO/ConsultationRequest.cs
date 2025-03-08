ba0536d65520e3e90b79e207205528c0aed7e451
MOHAMMED AFSAL N S <mohafsal890@gmail.com>
2025-03-08 11:05:33 +0530

    Revert "Admin changes added"

---
+++ DTO/ConsultationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CMSv2026WebApp.DTO
+{
+    public class ConsultationRequest
+    {
+        public int AppointmentId { get; set; }
+        public int DoctorId { get; set; }
+        public int PatientId { get; set; }
+        public string Diagnosis { get; set; }
+        public string Notes { get; set; }
+    }
+}
